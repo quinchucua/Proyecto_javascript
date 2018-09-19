7d2d79ea60cdeff70285c1d0debcc5da50ece517
quinchucua <43157300+quinchucua@users.noreply.github.com>
2018-09-19 15:13:08 -0500

    usuario y commit casi terminado, falta historial

--- app/scripts/controllers/users.js
+++ app/scripts/controllers/users.js
@@ -2,7 +2,8 @@
  * Función para agregar toda la logica de la administración 
  * usuarios
  */
-var a,b,c,d;
+
+Users={"first_name":"","Last_name":"","email":"","profession":""}
 function usersActionsController() {
     var a = document.querySelector("#guardar");
     a.addEventListener("click",Guardar);
@@ -12,10 +13,19 @@ function usersActionsController() {
     
 
     function Guardar(){
-       
-        var b= document.getElementById("usuario");
-        var c = document.getElementById("mostrar")
-        var d = document.getElementById("tabla");
+        b= document.getElementById("usuario");
+        c = document.getElementById("mostrar");
+        d = document.getElementById("tabla");
+        usuario=b.value;
+        nombre = usuario.split(" ");
+        Users["first_name"]=nombre[0];
+        Users["Last_name"]=nombre[1];
+        Users["email"]=document.getElementById("email").value;
+        Users["profesion"]=document.getElementById("profesion").value;
+        alert(Users.first_name+" nombre");
+        alert(Users.Last_name+ " Apellido");
+        alert(Users.email+ " correo");
+        alert(Users.profesion);
         d.style.display= 'block';
         c.innerHTML=b.value;
         b.value="";
--- app/scripts/controllers/versioner.js
+++ app/scripts/controllers/versioner.js
@@ -1,19 +1,71 @@
 /**
  * Función para manejar toda la logica de control de versiones
  */
-var a2,b2,c2;
-var texto,comando;
+
+var Commits ={"messaje":"" ,"hash":"","text_version":"","user_id":"" };
+var id =0;
+
 function versionerActionsController() {
     a2 = document.getElementById("ver-text");
     b2 = document.getElementById("commander");
-    b2.addEventListener("click",accion,true);
     c2 = document.getElementById("inp-command");
+    b2.addEventListener("click",accion,true);
+    usu="";
     
 
 }
 function accion(){
-    texto = a2.value;
-    comando = c2.value;
+    hash = GenerarHash();
+    
+    id = asignarid();
+    alert(id);
+    alert(hash); 
+    vc = c2.value;
+    var comando = vc.split(" -m");    
+    if(comando[0] == "commit"){
+        Commits["messaje"] = a2.value;
+        Commits["hash"]=hash;
+        Commits["user_id"]=id;
+        Commits["text_version"]=comando[1];
+        alert(Commits);
+        alert(Commits.hash);
+    }
 
-    alert(texto +" "+comando);
-}
+    }
+    function GenerarHash() {
+        var letras = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+        con =Math.round(aleatorionum(100,1000));
+        con2 =Math.round(aleatorionum(100,1000));
+        index = Math.round(aleatorioletra(0,26));
+        index2 = Math.round(aleatorioletra(0,26));
+        hash1= con+letras[index]+letras[index2]+con2;
+
+        function aleatorionum(min, max) {
+            return Math.random() * (max - min) + min; 
+          }
+          function aleatorioletra(min, max) {
+            return Math.random() * (max - min) + min; 
+          }
+          return hash1;
+
+     }
+     function asignarid(){
+         var cont = 0;
+       if (Users.first_name!=""){
+           
+           if (usu == Users.first_name ){
+               
+                return id;
+           }else{ 
+            usu = Users.first_name;         
+           if(id>0){
+               cont=id+1;
+            }else {
+                cont++;
+            }}
+           
+           return cont;
+       }
+       alert ("no se ha iniciado sesion") ;
+       return 0;
+     }
